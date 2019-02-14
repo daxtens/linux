66810dd8579107e04272ac3673b2d88156af9db1
Daniel Axtens <dja@axtens.net>
2019-02-14 16:08:44 +1100

    powerpc: KASAN for 64bit Book3E
    Wire up KASAN. Only outline instrumentation is supported.
    
    The KASAN shadow area is mapped into vmemmap space:
    0x8000 0400 0000 0000 to 0x8000 0600 0000 0000.
    To do this we require that vmemmap be disabled. (This is the default
    in the kernel config that QorIQ provides for the machine in their
    SDK anyway - they use flat memory.)
    
    Only the kernel linear mapping (0xc000...) is checked. The vmalloc and
    ioremap areas (also in 0x800...) are all mapped to the zero page. As
    with the Book3S hash series, this requires overriding the memory <->
    shadow mapping.
    
    Also, as with both previous 64-bit series, early instrumentation is not
    supported.  It would allow us to drop the check_return_arch_not_ready()
    hook in the KASAN core, but it's tricky to get it set up early enough:
    we need it setup before the first call to instrumented code like printk().
    Perhaps in the future.
    
    Only KASAN_MINIMAL works.
    
    Lightly tested on e6500. KVM, kexec and xmon have not been tested.
    
    The test_kasan module fires warnings as expected, except for the
    following tests:
    
     - Expected/by design:
    kasan test: memcg_accounted_kmem_cache allocate memcg accounted object
    
     - Due to only supporting KASAN_MINIMAL:
    kasan test: kasan_stack_oob out-of-bounds on stack
    kasan test: kasan_global_oob out-of-bounds global variable
    kasan test: kasan_alloca_oob_left out-of-bounds to left on alloca
    kasan test: kasan_alloca_oob_right out-of-bounds to right on alloca
    kasan test: use_after_scope_test use-after-scope on int
    kasan test: use_after_scope_test use-after-scope on array
    
    Thanks to those who have done the heavy lifting over the past several years:
     - Christophe's 32 bit series: https://lists.ozlabs.org/pipermail/linuxppc-dev/2019-February/185379.html
     - Aneesh's Book3S hash series: https://lwn.net/Articles/655642/
     - Balbir's Book3S radix series: https://patchwork.ozlabs.org/patch/795211/
    
    Cc: Christophe Leroy <christophe.leroy@c-s.fr>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Cc: Balbir Singh <bsingharora@gmail.com>
    Signed-off-by: Daniel Axtens <dja@axtens.net>
    
    ---
    
    While useful if you have a book3e device, this is mostly intended
    as a warm-up exercise for reviving Aneesh's series for book3s hash.
    In particular, changes to the kasan core are going to be required
    for hash and radix as well.

--- arch/powerpc/include/asm/kasan.h
+++ arch/powerpc/include/asm/kasan.h
@@ -18,10 +18,11 @@
 
 #define KASAN_SHADOW_SCALE_SHIFT	3
 
-#define KASAN_SHADOW_OFFSET	ASM_CONST(CONFIG_KASAN_SHADOW_OFFSET)
-
 #define KASAN_SHADOW_START	(KASAN_SHADOW_OFFSET + \
 				 (PAGE_OFFSET >> KASAN_SHADOW_SCALE_SHIFT))
+
+#ifdef CONFIG_PPC32
+#define KASAN_SHADOW_OFFSET	ASM_CONST(CONFIG_KASAN_SHADOW_OFFSET)
 
 #define KASAN_SHADOW_END	0UL
 
@@ -29,6 +30,70 @@
 
 #ifdef CONFIG_KASAN
 void kasan_early_init(void);
+#endif
+#endif /* CONFIG_PPC32 */
+
+#ifdef CONFIG_PPC_BOOK3E_64
+// don't put in Kconfig; will triger KASAN_MINIMAL false and explode
+#define KASAN_SHADOW_OFFSET ASM_CONST(0x6800040000000000)
+
+#define KASAN_SHADOW_SIZE	(KERN_VIRT_SIZE >> KASAN_SHADOW_SCALE_SHIFT)
+
+extern struct static_key_false powerpc_kasan_enabled_key;
+#define check_return_arch_not_ready() \
+	do {								\
+		if (!static_branch_likely(&powerpc_kasan_enabled_key))	\
+			return;						\
+	} while (0)
+
+extern unsigned char kasan_zero_page[PAGE_SIZE];
+static inline void *kasan_mem_to_shadow_book3e(const void *addr)
+{
+	if ((unsigned long)addr >= KERN_VIRT_START &&
+		(unsigned long)addr < (KERN_VIRT_START + KERN_VIRT_SIZE)) {
+		return (void *)kasan_zero_page;
+	}
+
+	return (void *)((unsigned long)addr >> KASAN_SHADOW_SCALE_SHIFT)
+		+ KASAN_SHADOW_OFFSET;
+}
+#define kasan_mem_to_shadow kasan_mem_to_shadow_book3e
+
+static inline void *kasan_shadow_to_mem_book3e(const void *shadow_addr)
+{
+	/*
+	 * We map the entire non-linear virtual mapping onto the zero page so if
+	 * we are asked to map the zero page back just pick the beginning of that
+	 * area.
+	 */
+	if (shadow_addr >= (void *)kasan_zero_page &&
+		shadow_addr < (void *)(kasan_zero_page + PAGE_SIZE)) {
+		return (void *)KERN_VIRT_START;
+	}
+
+	return (void *)(((unsigned long)shadow_addr - KASAN_SHADOW_OFFSET)
+		<< KASAN_SHADOW_SCALE_SHIFT);
+}
+#define kasan_shadow_to_mem kasan_shadow_to_mem_book3e
+
+static inline bool kasan_addr_has_shadow_book3e(const void *addr)
+{
+	/*
+	 * We want to specifically assert that the addresses in the 0x8000...
+	 * region have a shadow, otherwise they are considered by the kasan
+	 * core to be wild pointers
+	 */
+	if ((unsigned long)addr >= KERN_VIRT_START &&
+		(unsigned long)addr < (KERN_VIRT_START + KERN_VIRT_SIZE)) {
+		return true;
+	}
+	return (addr >= kasan_shadow_to_mem((void *)KASAN_SHADOW_START));
+}
+#define kasan_addr_has_shadow kasan_addr_has_shadow_book3e
+
+#endif /* CONFIG_PPC_BOOK3E_64 */
+
+#ifdef CONFIG_KASAN
 void kasan_init(void);
 #else
 static inline void kasan_init(void) { }
